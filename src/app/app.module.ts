94d7c82ea61a9758f805c1a08f737718b0c117e9
Evgenii Kozlov1 <Evgenii_Kozlov1@epam.com>
2019-05-25 22:37:14 +0400

    goods done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { MainComponent } from './common-modules/main/main.component';
+import { HeaderComponent } from './common-modules/header/header.component';
+import { FooterComponent } from './common-modules/footer/footer.component';
+import { GoodsCategoryComponent } from './core-modules/goods-category/goods-category.component';
+import { ButtonsModule } from 'ngx-bootstrap/buttons';
+import { PaginationModule } from 'ngx-bootstrap/pagination';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    HeaderComponent,
+    FooterComponent,
+    GoodsCategoryComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    ButtonsModule.forRoot(),
+    PaginationModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/core-modules/interfaces/goods-item.interface.ts
@@ -0,0 +1,5 @@
+export interface GoodsItem {
+    title: string;
+    description: string;
+    image: string;
+}
