48f378a03e385451ffcfb293812bf0eb5b49860c
ValentinPerov <valentin_perov@epam.com>
2019-05-27 02:46:23 +0400

    [WIP] Contacts

---
+++ src/app/core-modules/interfaces/goods-category.interface.ts
@@ -0,0 +1,8 @@
+import { GoodsItem } from './goods-item.interface';
+
+export interface GoodsCategory {
+    title: string;
+    text: string;
+    image: string;
+    // newsList: Array <NewsItem>;
+}
