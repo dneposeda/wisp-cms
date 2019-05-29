53a875b9cb23538662c222b452b1dcfc1524b269
Denis Tivikov <denis_tivikov@epam.com>
2019-05-29 20:18:21 +0400

    wisp#2-03 Add component "Share-btn"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { CommentItemComponent } from './components/comment-item/comment-item.component';
 import { CommentListComponent } from './components/comment-list/comment-list.component';
+import { ShareBtnComponent } from './components/share-btn/share-btn.component';
 
 @NgModule({
     declarations: [
         AppComponent,
         CommentItemComponent,
-        CommentListComponent
+        CommentListComponent,
+        ShareBtnComponent
     ],
     imports: [
         BrowserModule
