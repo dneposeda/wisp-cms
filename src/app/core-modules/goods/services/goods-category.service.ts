c06040f205dbbe66ef0db460950df225db1bfee1
Evgenii Kozlov1 <Evgenii_Kozlov1@epam.com>
2019-05-29 20:50:29 +0400

    murkup & lineup fixes

--- src/app/core-modules/goods/services/goods-category.service.ts
+++ src/app/core-modules/goods/services/goods-category.service.ts
@@ -6,19 +6,19 @@ import { GoodsCategoryPage } from '@app/core-modules/goods/interfaces/goods-cat-
 import { GoodsCategory } from '@app/core-modules/goods/common/interfaces/goods-category.interface';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class GoodsCategoryService {
     private apiUrl = 'assets/dev-stubs/goods-category';
     private pageUrl = 'goods-category-page.stub.json';
     private catListPath = 'goods-cat-list.stub.json';
 
-  constructor( private http: HttpClient ) {}
+    constructor( private http: HttpClient ) {}
 
-  public getGoodsPage(): Observable<GoodsCategoryPage> {
-      return this.http.get<GoodsCategoryPage>(this.apiUrl + '/' + this.pageUrl );
-  }
-  public getGoodsCatList(): Observable<GoodsCategory[]> {
-      return this.http.get<GoodsCategory[]>(this.apiUrl + '/' + this.catListPath);
-  }
+    public getGoodsPage(): Observable<GoodsCategoryPage> {
+        return this.http.get<GoodsCategoryPage>(this.apiUrl + '/' + this.pageUrl );
+    }
+    public getGoodsCatList(): Observable<GoodsCategory[]> {
+        return this.http.get<GoodsCategory[]>(this.apiUrl + '/' + this.catListPath);
+    }
 }
