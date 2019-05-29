4f7fb43102723e05be327bf7e2331906a9c99c53
Denis Tivikov <denis_tivikov@epam.com>
2019-05-29 20:10:35 +0400

    wisp#2-02 add component comment-list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { CommentItemComponent } from './components/comment-item/comment-item.component';
+import { CommentListComponent } from './components/comment-list/comment-list.component';
 
 @NgModule({
     declarations: [
         AppComponent,
-        CommentItemComponent
+        CommentItemComponent,
+        CommentListComponent
     ],
     imports: [
         BrowserModule
