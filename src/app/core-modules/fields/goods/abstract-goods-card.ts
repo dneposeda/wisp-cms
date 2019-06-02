2180eebd2ec645e4c9dd96ae171369270045dbea
Evgenii Kozlov1 <Evgenii_Kozlov1@epam.com>
2019-06-02 11:25:43 +0400

    abstraction added

---
+++ src/app/core-modules/fields/goods/abstract-goods-card.ts
@@ -0,0 +1,6 @@
+export class AbstractGoodsCard {
+    public id: number;
+    public title: string;
+    public image: string;
+    public description: string;
+}
---
+++ src/app/core-modules/fields/goods/goods-medium-card.ts
@@ -0,0 +1,2 @@
+import { AbstractGoodsCard } from './abstract-goods-card';
+export class GoodsMediumCard extends AbstractGoodsCard { }
