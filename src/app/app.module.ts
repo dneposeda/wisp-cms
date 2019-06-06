f1c44dd6f539b8bad4927b071bf182ae8796f07f
Evgenii Kozlov1 <Evgenii_Kozlov1@epam.com>
2019-06-06 16:31:53 +0400

    merge done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,15 +10,10 @@ import { PaginationModule } from 'ngx-bootstrap/pagination';
 import { FormsModule } from '@angular/forms';
 import { PaginationComponent } from './common-modules/pagination/pagination.component';
 import { NewsMediumCardComponent } from './core-modules/news/commons/components/news-medium-card/news-medium-card.component';
-import { SideColumnComponent } from './core-modules/news/commons/components/side-column/side-column.component';
-<<<<<<< HEAD
 import { NewsSmallCardComponent } from './core-modules/news/commons/components/news-small-card/news-small-card.component';
 import { NewsBigCardComponent } from './core-modules/news/commons/components/news-big-card/news-big-card.component';
-
-=======
 import { SideColumnComponent } from './core-modules/goods/common/components/side-column/side-column.component';
 import { GoodsMediumCardComponent } from './core-modules/goods/common/components/goods-medium-card/goods-medium-card.component';
->>>>>>> cc180614ee8b5937923d1c41829810f4a0b47529
 
 @NgModule({
   declarations: [
@@ -26,17 +21,11 @@ import { GoodsMediumCardComponent } from './core-modules/goods/common/components
     NewsCategoryComponent,
     PaginationComponent,
     NewsMediumCardComponent,
-<<<<<<< HEAD
     SideColumnComponent,
     NewsSmallCardComponent,
-    NewsBigCardComponent
-=======
-    SideColumnComponent
+    NewsBigCardComponent,
     GoodsCategoryComponent,
-    PaginationComponent,
-    SideColumnComponent,
     GoodsMediumCardComponent
->>>>>>> cc180614ee8b5937923d1c41829810f4a0b47529
   ],
   imports: [
     BrowserModule,
